c84be2773b5f393e69af953cec2b8cd77409aca2
ahmnot <othmanmoat@hotmail.fr>
2024-02-24 22:13:01 +0100

    began styling scrollbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,15 +48,6 @@
 		width: 100%;
 	}
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		width: 100%;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
 	@media (max-width: 400px) {
 		.media-grid {
 			display: grid;
