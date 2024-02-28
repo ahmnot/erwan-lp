a629af196ad72ae860cb5bf5482d90618e17b708
ahmnot <othmanmoat@hotmail.fr>
2024-02-28 21:02:05 +0100

    social bar is better

--- src/routes/SocialBar.svelte
+++ src/routes/SocialBar.svelte
@@ -19,45 +19,29 @@
 			isMailShown = !isMailShown;
 		}
 	}
-
-	function handleKeyDownPhone(event) {
-		if (event.key === 'Enter' || event.key === ' ') {
-			phoneClickHandler();
-		}
-	}
-
-	function handleKeyDownMail(event) {
-		if (event.key === 'Enter' || event.key === ' ') {
-			mailClickHandler();
-		}
-	}
 </script>
 
 <ul class="socials" class:socialsincontact>
-	<li>
-		<button
-			class="phone-icon-button"
-			on:click={phoneClickHandler}
-			on:keydown={handleKeyDownPhone}
-			tabindex="0"
-		>
-			<img class="phone-icon" src="/icons/phone.webp" alt="Phone Icon" />
-		</button>
+	<li class="phone-container">
 		{#if isPhoneShown || socialsincontact}
 			<div class="phone-box" transition:fade={{ duration: 60 }}>+33 6 47 86 26 23</div>
 		{/if}
+		<button
+			class="phone-icon-button"
+			on:click={phoneClickHandler}
+		>
+			<img class="phone-icon" src="/icons/phone.webp" alt="Phone Icon" />
+		</button>
 	</li>
-	<li>
+	<li class="email-container">
 		<button
 			class="mail-icon-button"
 			on:click={mailClickHandler}
-			on:keydown={handleKeyDownMail}
-			tabindex="0"
 		>
 			<img class="mail-icon" src="/icons/mail.webp" alt="Mail Icon" />
 		</button>
 		{#if isMailShown || socialsincontact}
-			<div class="email-box" transition:fade={{ duration: 60 }}>erwanlepape@outlook.com</div>
+			<div class="email-box" transition:fade={{ duration: 50 }}>erwanlepape@outlook.com</div>
 		{/if}
 	</li>
 	<li>
@@ -73,13 +57,59 @@
 </ul>
 
 <style>
-	button {
+
+	.phone-icon,
+	.mail-icon,
+	.soundcloud-icon,
+	.instagram-icon {
+		width: 32px; /* Adjust based on your preferred size */
+		height: 32px; /* Adjust based on your preferred size */
+		margin: auto; /* Center the icon */
+		display: block; /* Allows margin: auto to center the icon vertically */
 	}
 
-	img {
-		position: sticky;
-		width: 50%;
-		top: 50%;
+	/* Ensure the buttons are styled to center their content */
+	.phone-icon-button,
+	.mail-icon-button,
+	.instagram-icon-link,
+	.soundcloud-icon-link {
+		display: flex; /* Use flexbox to center content */
+		justify-content: center; /* Center horizontally */
+		align-items: center; /* Center vertically */
+		width: 50px; /* Adjust, ensure it's larger than the icon for padding */
+		height: 50px; /* Adjust, ensure it's larger than the icon for padding */
+		padding: 10px; /* Adjust or remove padding based on your design */
+	}
+	li.phone-container {
+		display: flex;
+		align-items: center; /* This will vertically center the items in the li */
+		justify-content: flex-start; /* Align items to the start of the li, which is the left side */
+		gap: 10px; /* This adds space between the phone-box and the phone-icon-button */
+	}
+
+	li.email-container {
+		position: relative; /* Add this to your existing li styles */
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.phone-box {
+		font-size: x-large;
+		white-space: nowrap;
+		opacity: 1;
+		color: white;
+	}
+
+	.email-box {
+		position: absolute;
+		top: 100%; /* Position the top of the email-box right at the bottom of the button */
+		left: 38%; /* Start at the horizontal center of the button */
+		transform: translate(-50%, 10px); /* Center it horizontally and move it 10px down */
+		font-size: x-large;
+		white-space: nowrap;
+		opacity: 1;
+		color: white;
 	}
 
 	.socials.socialsincontact {
@@ -107,48 +137,31 @@
 	}
 
 	li {
-		position: relative;
-		height: 100%;
+		display: flex; /* This makes sure the content of li is centered if needed */
+		align-items: center; /* Center vertically in case of different heights */
+		justify-content: center; /* Center horizontally */
 	}
 
-	.phone-box {
-		position: absolute;
-		top: 60%;
-		right: 120%;
-		transform: translateX(50%);
-		transform: translateY(-50%);
-		font-size: larger;
-		white-space: nowrap;
-		opacity: 1;
-		color: white;
-	}
-
-	.email-box {
-		position: absolute;
-		right: 77%;
-		transform: translate(50%);
-		font-size: larger;
-		opacity: 1;
-		color: white;
-	}
-
-	.phone-icon-button {
-		margin-right: 20px;
-		padding: 0px;
-		background: none;
-		border: none;
-	}
-
-	.phone-icon {
+	.phone-icon,
+	.mail-icon,
+	.instagram-icon,
+	.soundcloud-icon {
 		filter: grayscale(1);
 		transition: 0.1s filter linear;
 	}
 
-	.phone-icon:hover {
+	.phone-icon:hover,
+	.mail-icon:hover {
 		filter: grayscale(0);
 		cursor: pointer;
 	}
 
+	.instagram-icon:hover,
+	.soundcloud-icon:hover {
+		filter: grayscale(0);
+	}
+
+	.phone-icon-button,
 	.mail-icon-button {
 		margin-right: 20px;
 		padding: 0px;
@@ -156,41 +169,9 @@
 		border: none;
 	}
 
-	.mail-icon {
-		filter: grayscale(1);
-		transition: 0.1s filter linear;
-	}
-
-	.mail-icon:hover {
-		filter: grayscale(0);
-		cursor: pointer;
-	}
-
-	.instagram-icon-link {
-		margin-right: 20px;
-		padding: 0px;
-	}
-
-	.instagram-icon {
-		filter: grayscale(1);
-		transition: 0.1s filter linear;
-	}
-
-	.instagram-icon:hover {
-		filter: grayscale(0);
-	}
-
+	.instagram-icon-link,
 	.soundcloud-icon-link {
 		margin-right: 20px;
 		padding: 0px;
-	}
-
-	.soundcloud-icon {
-		filter: grayscale(1);
-		transition: 0.1s filter linear;
-	}
-
-	.soundcloud-icon:hover {
-		filter: grayscale(0);
 	}
 </style>
