e1f38942d991c902d349fe222792227b930da986
ahmnot <othmanmoat@hotmail.fr>
2024-03-06 17:55:12 +0100

    First changes made directly with Erwan

--- src/lib/mediaList.js
+++ src/lib/mediaList.js
@@ -2,98 +2,99 @@
 // src/lib/mediaList.js
 let imagePath = '/affiches-court-metrages/';
 
-export const mediaList = [{
-    id: 'fantasy',
-    images: [imagePath + 'fantasy.webp'],
-    title: 'Fantasy',
-    author: 'Erwan Le Pape',
-    work: 'Original music',
-    youtube: 'https://www.youtube.com/embed/9VjncpK4dV8',
-    soundcloud: 'https://soundcloud.com/erwanlepape/sets/fantasy-1',
-    soundcloudAPINumber: 'playlists/1772144829'
-},
-{
-    id: 'le-bouquet',
-    images: [imagePath + 'le-bouquet.webp'],
-    title: 'Le bouquet',
-    author: 'A movie by Anaïs Dusautois',
-    work: 'Rescoring',
-    youtube: 'https://www.youtube.com/embed/vMpwETdpQgs',
-    soundcloud: 'https://soundcloud.com/erwanlepape/le-bouquet',
-    soundcloudAPINumber: 'tracks/1671137550'
-},
-{
-    id: 'nature-morte',
-    images: [imagePath + 'nature-morte.webp'],
-    title: 'nature morte',
-    author: 'A movie by Alysée Lavallée-Imhof',
-    work: 'UQAM contest',
-    youtube: 'https://www.youtube.com/embed/aA2WnaCvRvw',
-    soundcloud: 'https://soundcloud.com/erwanlepape/nature-morte',
-    soundcloudAPINumber: 'tracks/1671138273'
-},
-{
-    id: 'resistencia',
-    images: [imagePath + 'resistencia.webp'],
-    title: 'Resistencia',
-    author: 'A movie by Flavia Carolliny Sena and Bruna Soares Pereira',
-    work: 'Original scoring',
-    youtube: 'https://www.youtube.com/embed/0KWvxGljza8',
-    soundcloud: 'https://soundcloud.com/erwanlepape/sets/resistencia',
-    soundcloudAPINumber: 'playlists/1762686030',
-    verticalOffset: '6.5%'
-},
-{
-    id: 'sahara',
-    images: [imagePath + 'sahara.webp'],
-    title: 'SAHARA',
-    author: 'A movie by MOPA',
-    work: 'Rescoring',
-    youtube: 'https://www.youtube.com/embed/iI65zBAHOpg',
-    soundcloud: 'https://soundcloud.com/erwanlepape/sets/sahara',
-    soundcloudAPINumber: 'playlists/1763727072'
-},
-{
-    id: 'time-trap',
-    images: [imagePath + 'time-trap.webp'],
-    title: 'Time Trap',
-    author: 'A movie by Michael Shanks',
-    work: 'IFMC contest',
-    youtube: 'https://www.youtube.com/embed/5Z-sKX2BtoA',
-    soundcloud: 'https://soundcloud.com/erwanlepape/ifmc-time-trap',
-    soundcloudAPINumber: 'tracks/1671134031',
-    verticalOffset: '-8%'
-},
-{
-    id: 'erinyes',
-    images: [imagePath + 'erinyes.webp'],
-    title: 'ERINYES',
-    author: 'A videogame by Gamagora',
-    work: 'Original Scoring, Sound Design',
-    youtube: '',
-    soundcloud: '',
-    soundcloudAPINumber: '',
-    horizontalOffset: '-5%'
-},
-{
-    id: 'glory-to-dallas',
-    images: [imagePath + 'glory-to-dallas.webp'],
-    title: 'Glory To Dallas',
-    author: 'A videogame by Brassart',
-    work: 'Original Scoring, Sound Design',
-    youtube: '',
-    soundcloud: '',
-    soundcloudAPINumber: '',
-    verticalOffset: '-18%'
-},
-{
-    id: 'bistro-belzebuth',
-    images: [imagePath + 'bistro-belzebuth.webp'],
-    title: 'Toy Tinkers',
-    author: 'An animation by Le Vieux Montréal',
-    work: 'Original Scoring',
-    youtube: '',
-    soundcloud: '',
-    soundcloudAPINumber: ''
-}
+export const mediaList = [
+    {
+        id: 'fantasy',
+        images: [imagePath + 'fantasy.webp'],
+        title: 'Fantasy',
+        author: 'Erwan Le Pape',
+        work: 'Original music',
+        youtube: 'https://www.youtube.com/embed/9VjncpK4dV8',
+        soundcloud: 'https://soundcloud.com/erwanlepape/sets/fantasy-1',
+        soundcloudAPINumber: 'playlists/1772144829'
+    },
+    {
+        id: 'sahara',
+        images: [imagePath + 'sahara.webp'],
+        title: 'SAHARA',
+        author: 'A movie by MOPA',
+        work: 'Rescoring',
+        youtube: 'https://www.youtube.com/embed/iI65zBAHOpg',
+        soundcloud: 'https://soundcloud.com/erwanlepape/sets/sahara',
+        soundcloudAPINumber: 'playlists/1763727072'
+    },
+    {
+        id: 'nature-morte',
+        images: [imagePath + 'nature-morte.webp'],
+        title: 'nature morte',
+        author: 'A movie by Alysée Lavallée-Imhof',
+        work: 'UQAM contest',
+        youtube: 'https://www.youtube.com/embed/aA2WnaCvRvw',
+        soundcloud: 'https://soundcloud.com/erwanlepape/nature-morte',
+        soundcloudAPINumber: 'tracks/1671138273'
+    },
+    {
+        id: 'time-trap',
+        images: [imagePath + 'time-trap.webp'],
+        title: 'Time Trap',
+        author: 'A movie by Michael Shanks',
+        work: 'IFMC contest',
+        youtube: 'https://www.youtube.com/embed/5Z-sKX2BtoA',
+        soundcloud: 'https://soundcloud.com/erwanlepape/ifmc-time-trap',
+        soundcloudAPINumber: 'tracks/1671134031',
+        verticalOffset: '-8%'
+    },
+    {
+        id: 'resistencia',
+        images: [imagePath + 'resistencia.webp'],
+        title: 'Resistencia',
+        author: 'A movie by Flavia Carolliny Sena and Bruna Soares Pereira',
+        work: 'Original scoring',
+        youtube: 'https://www.youtube.com/embed/0KWvxGljza8',
+        soundcloud: 'https://soundcloud.com/erwanlepape/sets/resistencia',
+        soundcloudAPINumber: 'playlists/1762686030',
+        verticalOffset: '6.5%'
+    },
+    {
+        id: 'le-bouquet',
+        images: [imagePath + 'le-bouquet.webp'],
+        title: 'Le bouquet',
+        author: 'A movie by Anaïs Dusautois',
+        work: 'Rescoring',
+        youtube: 'https://www.youtube.com/embed/vMpwETdpQgs',
+        soundcloud: 'https://soundcloud.com/erwanlepape/le-bouquet',
+        soundcloudAPINumber: 'tracks/1671137550'
+    },
+    {
+        id: 'erinyes',
+        images: [imagePath + 'erinyes.webp'],
+        title: 'ERINYES',
+        author: 'A videogame by Gamagora',
+        work: 'Original Scoring, Sound Design',
+        youtube: '',
+        soundcloud: '',
+        soundcloudAPINumber: '',
+        horizontalOffset: '-5%'
+    },
+    {
+        id: 'glory-to-dallas',
+        images: [imagePath + 'glory-to-dallas.webp'],
+        title: 'Glory To Dallas',
+        author: 'A videogame by Brassart',
+        work: 'Original Scoring, Sound Design',
+        youtube: '',
+        soundcloud: '',
+        soundcloudAPINumber: '',
+        verticalOffset: '-18%'
+    },
+    {
+        id: 'bistro-belzebuth',
+        images: [imagePath + 'bistro-belzebuth.webp'],
+        title: 'Toy Tinkers',
+        author: 'An animation by Le Vieux Montréal',
+        work: 'Original Scoring',
+        youtube: '',
+        soundcloud: '',
+        soundcloudAPINumber: ''
+    }
 ];
