d216d727eb528ecc513ea505421ed2db4cdd24e5
ahmnot <othmanmoat@hotmail.fr>
2024-11-13 20:07:32 +0100

    IMDB icon position.

--- src/routes/IconsBar.svelte
+++ src/routes/IconsBar.svelte
@@ -27,6 +27,11 @@
 		{/if}
 	</li>
 	<li>
+		<a class="icon-button-or-link" href="https://www.imdb.com/name/nm16474305/" target="_blank">
+				<img class="icon-normal icon-bigger" src="/icons/imdb.webp" alt="Discord Link" />
+		</a>
+	</li>
+	<li>
 		<a class="icon-button-or-link" href="https://soundcloud.com/erwanlepape" target="_blank">
 				<img class="icon-normal" src="/icons/soundcloud.webp" alt="Soundcloud Link" />
 		</a>
@@ -34,11 +39,6 @@
 	<li>
 		<a class="icon-button-or-link" href="https://www.instagram.com/erwanklp/" target="_blank">
 				<img class="icon-normal" src="/icons/instagram.webp" alt="Instagram Link" />
-		</a>
-	</li>
-	<li>
-		<a class="icon-button-or-link" href="https://www.imdb.com/name/nm16474305/" target="_blank">
-				<img class="icon-normal icon-bigger" src="/icons/imdb.webp" alt="Discord Link" />
 		</a>
 	</li>
 	<li>
